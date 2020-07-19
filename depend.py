bf3640125c55449fcce08d8f9dce2a20e0d97d12
Shubham Gupta <shubhamgupta20041998@gmail.com>
2020-07-20 00:55:58 +0530

    updating requirements.txt

--- depend.py
+++ depend.py
@@ -2,16 +2,15 @@ import nltk
 import string
 pre=''
 import speech_recognition as sr
-import pypiwin32
-import urllib.request, urllib.parse, urllib.error
-import urllib.request, urllib.error, urllib.parse
+import pywin32
+import urllib3
 import requests
 import twilio.rest
 
 print (nltk.__version__)
 #print (string.__version__)
-#print (speech_recognition.__version__)
-print (pypiwin32.__version__)
-#print (urllib.__version__)
+print (speech_recognition.__version__)
+print (pywin32.__version__)
+print (urllib3.__version__)
 print (requests.__version__)
 print (twilio.__version__)
