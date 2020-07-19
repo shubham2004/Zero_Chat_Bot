942311d5531ba34d0badea7124dde8151e246d7b
Shubham Gupta <shubhamgupta20041998@gmail.com>
2020-07-20 00:42:56 +0530

    updating requirements.txt

---
+++ depend.py
@@ -0,0 +1,17 @@
+import nltk
+import string
+pre=''
+import speech_recognition as sr
+import pypiwin32
+import urllib.request, urllib.parse, urllib.error
+import urllib.request, urllib.error, urllib.parse
+import requests
+import twilio.rest
+
+print (nltk.__version__)
+#print (string.__version__)
+#print (speech_recognition.__version__)
+print (pypiwin32.__version__)
+#print (urllib.__version__)
+print (requests.__version__)
+print (twilio.__version__)
